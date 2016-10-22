28f1dea32c638610d2e9c63e93072aa188195559
Antonio Jiménez <ajimenez.bf@gmail.com>
2016-10-23 01:09:19 +0200

    Single-post route, logic and view

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Post
 
@@ -7,3 +7,8 @@ def index(request):
     posts = Post.objects.order_by('-created')
     context = {'posts': posts}
     return render(request, 'app/index.html', context)
+
+
+def single_post(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, 'app/single_post.html', {'post': post})
