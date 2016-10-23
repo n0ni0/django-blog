26067c821111e0a34904db691b8b35b494790008
Antonio Jiménez <ajimenez.bf@gmail.com>
2016-10-23 20:00:43 +0200

    Configure admin site

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Category
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+
+admin.site.site_title = 'Blog'
+admin.site.site_header = 'Blog'
