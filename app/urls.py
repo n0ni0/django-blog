b4448c2ee83d4c94366d3f85b0a58595be017a05
Antonio Jiménez <ajimenez.bf@gmail.com>
2016-10-23 18:09:47 +0200

    Created category views

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<slug>[\w-]+)/$', views.single_post, name='single_post'),
+    url(r'^(?P<category>[\w-]+)$', views.show_category, name='category'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 
-from .models import Post
+from .models import Post, Category
 
 
 def index(request):
@@ -12,3 +12,12 @@ def index(request):
 def single_post(request, slug):
     post = get_object_or_404(Post, slug=slug)
     return render(request, 'app/single_post.html', {'post': post})
+
+
+def show_category(request, category):
+    categories = Category.objects.filter(slug=category)
+    posts = Post.objects.filter(category=categories)
+    return render(request, 'app/categories.html', {
+        'posts': posts,
+        'category': category
+    })
